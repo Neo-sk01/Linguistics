13061a6bf9ec88f58c2c2c61c6963801a3a508e6
Neosk <neosekaleli@gmail.com>
2025-09-12 20:04:22 +0200

    removed pieces on the navabar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -50,22 +50,9 @@ const routeList: RouteProps[] = [
     href: "#services",
     label: "Services",
   },
-
   {
     href: "/faq",
     label: "FAQ",
-  },
-  {
-    href: "/upload-test",
-    label: "Upload Files",
-  },
-  {
-    href: "/files",
-    label: "View Files",
-  },
-  {
-    href: "/admin",
-    label: "Admin",
   },
   {
     href: "/contact",
