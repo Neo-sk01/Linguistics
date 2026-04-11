3c63fd7efd8265c32065dccf79b6b667b417d77b
Neo Sekaleli <neosekaleli@gmail.com>
2026-04-11 10:14:37 +0200

    refactor: align sentry runtime config

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,28 +3,13 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { sentryBaseConfig } from "./lib/sentry-config";
 
 Sentry.init({
-  dsn: "https://f8b0c4aeb1efd10a4dd44c65bcefee3c@o4509518279409664.ingest.us.sentry.io/4509654657138688",
-
-  // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
+  ...sentryBaseConfig,
+  integrations: [Sentry.replayIntegration()],
   replaysSessionSampleRate: 0.1,
-
-  // Define how likely Replay events are sampled when an error occurs.
   replaysOnErrorSampleRate: 1.0,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,13 +3,6 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { sentryBaseConfig } from "./lib/sentry-config";
 
-Sentry.init({
-  dsn: "https://f8b0c4aeb1efd10a4dd44c65bcefee3c@o4509518279409664.ingest.us.sentry.io/4509654657138688",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+Sentry.init(sentryBaseConfig);
